0e810430eef21248db0c6d6f2a12681f63379e81
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-26 15:38:36 -0500

    Add operate function

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
+
+let first = "";
+let operator = "";
+let second = "";
+
+const operate =  (operation, number1, number2) => {
+    switch(operation){
+        case '+':{
+            add(number1,number2);
+            break;
+        }
+        case '-':{
+            subtract(number1,number2);
+            break;
+        }
+        case '*':{
+            multiply(number1,number2);
+            break;
+        }
+        case '/':{
+            divide(number1,number2);
+            break;
+        }
+    }
+};
