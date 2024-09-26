1e69aa8da87425a6277467634564b7c5afd1db54
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-26 15:57:10 -0500

    Add buttons to html and start styling

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+//Create operator functions
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
+//get Dom elements
+const numbers = document.getElementById("numbers");
+const numberBtn = document.createElement("button");
+
+
+for(let i = 9; i >= 0; i--){
+    numberBtn.textContent = i;
+    numbers.appendChild(numberBtn.cloneNode(true));
+}
+
+//Create variables
 let first = "";
 let operator = "";
 let second = "";
 
+//Create final operate function
 const operate =  (operation, number1, number2) => {
     switch(operation){
         case '+':{
