32d40604a3e54854db59d44423cbeaa46206226f
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-26 17:18:56 -0500

    Make calculator functional

--- script.js
+++ script.js
@@ -4,39 +4,102 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
-//get Dom elements
-const numbers = document.getElementById("numbers");
-const numberBtn = document.createElement("button");
+//Create final operate function
+const operate =  (operation, number1, number2) => {
+    switch(operation){
+        case '+':{
+            return add(number1,number2);
+            break;
+        }
+        case '-':{
+            return subtract(number1,number2);
+            break;
+        }
+        case '*':{
+            return multiply(number1,number2);
+            break;
+        }
+        case '/':{
+            return divide(number1,number2);
+            break;
+        }
+    }
+};
 
+const updateOperationScreen = (text) => {
+    if(text === ""){
+        operationScreen.textContent = text;
+        wholeOperation = text;
+    }else{
+        operationScreen.textContent += text;
+        wholeOperation += text;
+    }
+    
+}
 
-for(let i = 9; i >= 0; i--){
-    numberBtn.textContent = i;
-    numbers.appendChild(numberBtn.cloneNode(true));
+const updateAnswerScreen = (text) => {
+    answerScreen.textContent = "";
+    answerScreen.textContent = text;
 }
 
 //Create variables
 let first = "";
 let operator = "";
 let second = "";
+let wholeOperation =""
 
-//Create final operate function
-const operate =  (operation, number1, number2) => {
-    switch(operation){
-        case '+':{
-            add(number1,number2);
-            break;
-        }
-        case '-':{
-            subtract(number1,number2);
-            break;
-        }
-        case '*':{
-            multiply(number1,number2);
-            break;
-        }
-        case '/':{
-            divide(number1,number2);
-            break;
-        }
-    }
-};
+//get Dom elements
+const numbersDiv = document.getElementById("numbers");
+const numberBtn = document.createElement("button");
+const operationScreen = document.getElementById("operation");
+const answerScreen = document.getElementById("answer");
+
+
+for(let i = 9; i >= 0; i--){
+    numberBtn.textContent = i;
+    numberBtn.setAttribute("class", `number`);
+    numbersDiv.appendChild(numberBtn.cloneNode(true));
+}
+
+//get buttons
+const operands = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+//operands
+operands.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        updateOperationScreen(btn.textContent);
+    });
+});
+
+operators.forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+        const target = e.target
+        if(target.id === "clear"){
+            //Clear Screen
+            updateOperationScreen("");
+            updateAnswerScreen("");
+        }else if(target.id === "equal"){
+            //Run operation
+            wholeOperation = wholeOperation.split("?");
+            first = Number(wholeOperation[0]);
+            operator = wholeOperation[1];
+            second = Number(wholeOperation[2]);
+            const result = operate(operator, first,second);
+
+            //Update screens
+            updateOperationScreen("");
+            updateAnswerScreen(result);
+
+        }else{
+            //Add deliminator
+            wholeOperation += "?"
+            operationScreen.textContent += btn.textContent;
+            wholeOperation += btn.textContent;
+            wholeOperation += "?"
+        }       
+    });
+});
+
+
+
